0902589bdbdf6f0837b642d493c152000e09033b
Robin Janke <mail@robin-janke.de>
2024-12-08 19:45:31 +0100

    changes

--- src/services/git.ts
+++ src/services/git.ts
@@ -17,35 +17,44 @@ export class GitService {
     async updateImageTag(currentConfiguration: CurrentConfiguration, newImageTag: string): Promise<void> {
 
         /// Set the variables
-        const tempGitDirectory = tmp.dirSync({mode: 755, keep: true}).name;
-        const absoluteValuesPath = path.join(tempGitDirectory, currentConfiguration.environment.name);
-
-        // Init git
-        const git: SimpleGit = simpleGit({
-            config: [
-                `user.name=${currentConfiguration.git_configuration!.credentials.username}`,
-                `user.email=${currentConfiguration.git_configuration!.credentials.email}`
-            ],
-            binary: '/usr/bin/git',
-            baseDir: tempGitDirectory,
-            maxConcurrentProcesses: 10
-        });
+        const tempGitDirectory = path.join(tmp.dirSync().name, new Date().getTime().toString());
+        const absoluteValuesPath = path.join(tempGitDirectory, currentConfiguration.environment.values_file);
 
         // Clone the repository
-        logger.info("Cloning into " + tempGitDirectory);
-        await git.clone(this.buildGitRemoteUrl(currentConfiguration.git_configuration!.credentials, currentConfiguration.git_configuration!.repository), tempGitDirectory, ['--branch', currentConfiguration.git_configuration!.repository.default_branch]);
-        logger.info("Successfully cloned repository");
+        const url = this.buildGitRemoteUrl(currentConfiguration.git_configuration!.credentials, currentConfiguration.git_configuration!.repository);
+        const branch = currentConfiguration.git_configuration!.repository.default_branch;
+        const gitCommand = `git clone ${url} ${tempGitDirectory} --branch ${branch}`;
+        const gitOutput = shell.exec(gitCommand);
+        if (gitOutput.code !== 0) {
+            throw new Error(`Failed to clone repository: ${gitOutput.stderr}`);
+        }
 
-        // Read the current values file and update it
+        // Update the image tag in the values file
         const command = `yq -i e '.${currentConfiguration.environment.values_file}|="${newImageTag}"' ${absoluteValuesPath}`;
         const output = shell.exec(command);
         if (output.code !== 0) {
             throw new Error(`Failed to update image tag in values file: ${output.stderr}`);
         }
 
-        // Commit and push changes
-        await git.add(absoluteValuesPath);
-        await git.commit(`BOT: Update ${currentConfiguration.project.id} (${currentConfiguration.environment.name}) to ${newImageTag}`);
-        await git.push();
+        // Add the changes to git
+        const gitAddCommand = `cd ${tempGitDirectory} && git add .`;
+        const gitAddOutput = shell.exec(gitAddCommand);
+        if (gitAddOutput.code !== 0) {
+            throw new Error(`Failed to add changes to git: \nstdout:${gitAddOutput.stderr}\nstderr:${gitAddOutput.stderr}`);
+        }
+
+        // Commit the changes
+        const gitCommitCommand = `cd ${tempGitDirectory} && git commit -m "BOT: Update ${currentConfiguration.project.id} (${currentConfiguration.environment.name}) to ${newImageTag}"`;
+        const gitCommitOutput = shell.exec(gitCommitCommand);
+        if (gitCommitOutput.code !== 0) {
+            throw new Error(`Failed to commit changes to git: \nstdout:${gitCommitOutput.stderr}\nstderr:${gitCommitOutput.stderr}`);
+        }
+
+        // Push the changes
+        const gitPushCommand = `cd ${tempGitDirectory} && git push`;
+        const gitPushOutput = shell.exec(gitPushCommand);
+        if (gitPushOutput.code !== 0) {
+            throw new Error(`Failed to push changes to git: \nstdout:${gitPushOutput.stderr}\nstderr:${gitPushOutput.stderr}`);
+        }
     }
 }
